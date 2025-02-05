5ba4b6b694a64e23088d42485d9d3b7a77f7212a
Karthik Nambiar <111984323+Karthik2370@users.noreply.github.com>
2025-02-05 17:33:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Use your PNG favicon */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <title>CBX Logistics Tracker</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
-
-
