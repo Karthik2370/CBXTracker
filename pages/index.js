c5fc02367e697e347bc32cd07a431a56a717aba1
Karthik Nambiar <111984323+Karthik2370@users.noreply.github.com>
2025-02-06 15:01:53 +0530

    name change

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
         
         {/* Copyright */}
         <div className="text-center sm:text-left mb-2 sm:mb-0">
-          &copy; {new Date().getFullYear()} CBX Logistics. All rights reserved. Made by Karthik
+          &copy; {new Date().getFullYear()} CBX Logistics. All rights reserved. Made by Karthik Nambiar
         </div>
 
         {/* About CBX */}
