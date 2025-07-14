f5792a8f6cf2919e53bfbd1191a842dc85e53370
Karthik Nambiar <111984323+Karthik2370@users.noreply.github.com>
2025-07-14 18:39:02 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,15 +5,17 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWrzazfJ0XppB_UMZhYv-LU18Yjlq-jxQ",
-  authDomain: "cbx-logistics-website.firebaseapp.com",
-  projectId: "cbx-logistics-website",
-  storageBucket: "cbx-logistics-website.firebasestorage.app",
-  messagingSenderId: "318828955558",
-  appId: "1:318828955558:web:13554df63818500192b167"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);       // For authentication (login)
 export const db = getFirestore(app);    // For Firestore database
+
